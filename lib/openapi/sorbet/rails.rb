3fc601d59e5e0b3656fdcab55f755770d8986d29
Shuhei YOSHIDA <contact@yantene.net>
2024-02-14 01:06:50 +0900

    build: initial commit

---
+++ lib/openapi/sorbet/rails.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "rails/version"
+
+module Openapi
+  module Sorbet
+    module Rails
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/openapi/sorbet/rails/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Openapi
+  module Sorbet
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
